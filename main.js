dd57b05d69af360f7cdcd0b83b5afb8d00a1ce64
Scott Humphries <git@sscotth.io>
2017-01-19 12:11:47 -0600

    complete weather controller/actory

--- main.js
+++ main.js
@@ -15,6 +15,26 @@ angular
     console.log('I am a RootCtrl')
     $scope.gotoWeather = () => $location.url(`/weather/${$scope.zip}`)
   })
-  .controller('WeatherCtrl', function () {
+  .controller('WeatherCtrl', function ($scope, $routeParams, weatherFactory) {
     console.log('I am a WeatherCtrl')
+
+    weatherFactory
+      .getWeather($routeParams.zipcode)
+      .then((weather) => {
+        $scope.temperature = weather.temp
+        $scope.city = weather.city
+      })
+  })
+  .factory('weatherFactory', ($http) => {
+    return {
+      getWeather (zipcode) {
+        return $http
+          .get(`http://api.wunderground.com/api/69e0974e13868fe4/conditions/q/${zipcode}.json`)
+          .then((response) => ({ // same as `=> return {`
+              temp: response.data.current_observation.temp_f,
+              city: response.data.current_observation.display_location.full,
+            })
+          )
+      },
+    }
   })
