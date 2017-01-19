9d9ebb76786a4c06eb3cd1e7ae2407bad9e6c6da
Scott Humphries <git@sscotth.io>
2017-01-19 11:10:58 -0600

    weather route

--- main.js
+++ main.js
@@ -6,7 +6,15 @@ angular
         controller: 'RootCtrl',
         templateUrl: '/partials/root.html',
       })
+      .when('/weather/:zipcode', {
+        controller: 'WeatherCtrl',
+        templateUrl: '/partials/weather.html',
+      })
   })
-  .controller('RootCtrl', function () {
+  .controller('RootCtrl', function ($scope, $location) {
     console.log('I am a RootCtrl')
+    $scope.gotoWeather = () => $location.url(`/weather/${$scope.zip}`)
+  })
+  .controller('WeatherCtrl', function () {
+    console.log('I am a WeatherCtrl')
   })
