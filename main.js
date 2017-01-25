3dcdf65cc1d5991c048a2d86748361fb6407c07c
Scott Humphries <git@sscotth.io>
2017-01-25 11:12:08 -0600

    WIP: angular auth routing

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 angular
   .module('anguweathar', ['ngRoute'])
   .config(($routeProvider) => {
+    console.log('Config executing')
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyDfKMbHscf_4oQ5oL0EGh0u6zP9Gunp9l0",
+      authDomain: "c17-jquery-auth.firebaseapp.com",
+      databaseURL: "https://c17-jquery-auth.firebaseio.com",
+      storageBucket: "c17-jquery-auth.appspot.com",
+      messagingSenderId: "936314115006"
+    })
+
+    const checkForAuth = {
+      checkForAuth: function ($location) {
+        console.log('This will fire just before the WeatherCtrl')
+        if (firebase.auth().currentUser === null) {
+          $location.url('/')
+        }
+      }
+    }
+
     $routeProvider
       .when('/', {
         controller: 'RootCtrl',
@@ -9,14 +28,31 @@ angular
       .when('/weather/:zipcode', {
         controller: 'WeatherCtrl',
         templateUrl: '/partials/weather.html',
+        resolve: checkForAuth
+        // resolve takes an object with a function inside
+        // https://docs.angularjs.org/api/ngRoute/provider/$routeProvider#when
       })
+  })
+  .run(($location) => {
+    console.log('Run Executing')
+
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        // ???
+        console.log('Logged In', user)
+      } else {
+
+      }
+    })
   })
   .controller('RootCtrl', function ($scope, $location) {
     console.log('I am a RootCtrl')
+    console.log('Current user', firebase.auth().currentUser)
     $scope.gotoWeather = () => $location.url(`/weather/${$scope.zip}`)
   })
   .controller('WeatherCtrl', function ($scope, $routeParams, weatherFactory) {
     console.log('I am a WeatherCtrl')
+    console.log('Current user', firebase.auth().currentUser)
 
     weatherFactory
       .getWeather($routeParams.zipcode)
